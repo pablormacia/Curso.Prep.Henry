93fd70504c05c32769202d998e22100807981a04
pablormacia <pablo-macia@live.com.ar>
2021-06-24 11:56:16 -0300

    Finalizada lección 04-JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,7 +26,7 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-  for (var i; i<array.length;i++){
+  for (var i=0; i<array.length;i++){
     array[i]=array[i]+1;
   }
   return array;
@@ -129,12 +129,28 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length==0){
+    return 0;
+  }else{
+    var producto=1;
+    for(var i=0;i<arguments.length;i++){
+      producto*=arguments[i];
+    }
+    return producto;
+  }
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var cuenta=0;
+  for(var i=0;i<arreglo.length;i++){
+    if(arreglo[i]>18){
+      cuenta++;
+    }
+  }
+  return cuenta;
 
 }
 
@@ -144,6 +160,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if(numeroDeDia==1 || numeroDeDia==7){
+    return "Es fin de semana";
+  }else if(numeroDeDia<=6 && numeroDeDia>=2){
+    return "Es dia Laboral";
+  }
   
 } 
 
@@ -152,6 +173,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  n=n.toString();
+  if(n[0]==9){
+    return true
+  }else{
+    return false;
+  }
   
 }
 
@@ -160,6 +187,15 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+
+  for(var i=0;i<arreglo.length;i++){
+    if(arreglo[i+1]!=arreglo[i]){
+      return false;
+    }
+    return true;
+
+  }
+
   
 } 
 
@@ -169,6 +205,18 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray=[];
+  for(var i=0;i<array.length;i++){
+    if(array[i]=="Enero" || array[i]=="Marzo" || array[i]=="Noviembre"){
+      nuevoArray.push(array[i]);
+    }
+    
+  }
+  if (nuevoArray.length==3){
+    return nuevoArray;
+  }else{
+    return "No se encontraron los meses pedidos";
+  }
 }
 
 
@@ -176,6 +224,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray=[];
+  for(var i=0;i<array.length;i++){
+    if(array[i]>100){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
@@ -187,6 +242,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array=[];
+  var i=0;
+  var suma=numero;
+  while(i<10){
+    if(suma==i){
+      return "Se interrumpió la ejecución";
+    }
+    suma=suma+2;
+    array[i]=suma;
+    i++;
+  }
+  return array;
 }
 
 
@@ -197,6 +264,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array=[];
+  var i=0;
+  while(i<10){
+    i++;
+    if(i==4){
+      continue;
+    }else{
+    numero=numero+2;
+    array.push(numero);
+    }
+    
+  }
+  return array;
 }
 
 
